5227233e000acf1f842924f1abd5a40f6db2a617
hechaogm <hechaogm@gmail.com>
2023-02-16 18:09:28 +0800

    update

---
+++ MDK-ARM-migrate/MDK-ARM/UCCBEmbedded/stm32f0xx_hal_msp.d
@@ -0,0 +1,2 @@
+uccbembedded/stm32f0xx_hal_msp.o: ..\Src\stm32f0xx_hal_msp.c \
+  ..\Inc\main.h
---
+++ MDK-ARM-migrate/MDK-ARM/UCCBEmbedded/stm32f0xx_it.d
@@ -0,0 +1,2 @@
+uccbembedded/stm32f0xx_it.o: ..\Src\stm32f0xx_it.c ..\Inc\main.h \
+  ..\Inc\stm32f0xx_it.h
